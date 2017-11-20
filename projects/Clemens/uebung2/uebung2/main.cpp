4d5e0e190b7eb810d474b53c3cab76818074696d
Clemens Reinhardt <clemens.reinhardt@mailbox.tu-dresden.de>
2017-11-20 14:47:58 +0100

    neue version

--- projects/Clemens/uebung2/uebung2/main.cpp
+++ projects/Clemens/uebung2/uebung2/main.cpp
@@ -1,26 +1,43 @@
-/*
- * uebung2.cpp
- *
- * Created: 06.11.2017 16:04:59
- * Author : Clemens
- */ 
+#define F_CPU 16000000UL
 
-#include <avr/io.h>
-function GPIOInit;
-
-bool pressed;
-{
-
+extern "C" {
+	#include <avr/io.h>
+	#include <util/delay.h>
+	#include <stdint.h>
+	#include <util/delay.h>
+};
+void Blink( int number){
+	for (int i=0; i < number; i++){
+	PORTD |= 0b00000001;
+	_delay_ms(500);
+	PORTD &= 0b11111110;
+	_delay_ms(500);
+	}
 }
+
+void GPIO_Init();
+
 
 int main(void)
 {
-int count;
-count = 0;
-    /* Replace with your application code */
-    while (1) 
-    {
+	uint16_t counter {0};
+	
+	GPIO_Init();
 
-    }
+	while (1){
+		if (!(PIND & 0b10)) { // wenn taster nicht gedr??
+			counter ++;
+			Blink(counter);
+			}
+		else
+			Blink(0);
+			
+		
+	}
 }
 
+
+void GPIO_Init(){
+	DDRD |= 0b00000001;
+	PORTD |= 0b00000010;
+}
